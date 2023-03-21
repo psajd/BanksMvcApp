e59495ba5fdeb4e21b9c2b5416d8909adc4adef0
Gomzyakov I. A <76881688+psajd@users.noreply.github.com>
2023-03-21 22:07:33 +0300

    feat: NotificationController finished

--- src/main/java/com/psajd/banks/controllers/NotificationController.java
+++ src/main/java/com/psajd/banks/controllers/NotificationController.java
@@ -1,10 +1,32 @@
 package com.psajd.banks.controllers;
 
+import com.psajd.banks.services.BankService;
+import com.psajd.banks.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/banks/{bankId}/clients/{clientId}/notifications")
 public class NotificationController {
+    private final ClientService clientService;
+    private final BankService bankService;
 
+    @Autowired
+    public NotificationController(ClientService clientService, BankService bankService) {
+        this.clientService = clientService;
+        this.bankService = bankService;
+    }
+
+
+    @GetMapping("")
+    public String getNotifications(Model model, @PathVariable String bankId, @PathVariable String clientId) {
+        model.addAttribute("client", clientService.getClient(bankService.getBank(UUID.fromString(bankId)), UUID.fromString(clientId)));
+        return "notifications/notification";
+    }
 }
--- src/main/java/com/psajd/banks/core/notifications/Notification.java
+++ src/main/java/com/psajd/banks/core/notifications/Notification.java
@@ -3,12 +3,14 @@ package com.psajd.banks.core.notifications;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 /**
  * notification, it is used for transmission to the observer method
  */
 @Getter
+@Setter
 @AllArgsConstructor
 @ToString
 public class Notification {
