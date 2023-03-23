0a25c61a97acdacf6125c0f1ad8ae43c7bdadc69
Gomzyakov I. A <76881688+psajd@users.noreply.github.com>
2023-03-23 18:49:43 +0300

    fix: trying to connect with db

--- src/main/java/com/psajd/banks/controllers/TransactionController.java
+++ src/main/java/com/psajd/banks/controllers/TransactionController.java
@@ -1,9 +1,21 @@
 package com.psajd.banks.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/transaction")
+@RequestMapping("/transactions")
 public class TransactionController {
+
+    @GetMapping("")
+    public String getTransactionForm() {
+        return "transactions/transaction";
+    }
+
+    @PostMapping("")
+    public String doTransaction() {
+        return "redirect:/transactions";
+    }
 }
